48feda0e9669cdcc2133dc3d74a461f9daaf7b7f
Gousetaj <gousetaj.official@gmail.com>
2026-07-23 16:51:53 +0530

    docs: confirm direct state mutation doesn't trigger re-render, must use setter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,6 +4,7 @@ function Counter(){
     const [count,setCount]=useState(0)
     return <>
     Count:{count}
+    {/* <button onClick={()=>{count = count+1}}>+</button> */}
     <button onClick={()=>setCount(count+1)}>+</button>
     <button onClick={()=>setCount(count-1)}>-</button>
     <button onClick={()=>setCount(0)}>reset</button>
